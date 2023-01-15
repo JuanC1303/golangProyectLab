5b9e38d7da3775a96fd8079108cb4292199f7a94
JuanC1303 <ayalacortes13@gmail.com>
2023-01-14 23:18:30 -0600

    test/go.mod added

--- Task/Sum_test.go
+++ Task/Sum_test.go
@@ -2,10 +2,26 @@ package main
 
 import "testing"
 
-func testSum(t *testing.T) {
+func TestSum(t *testing.T) {
 
-	if sum1([]int{1, 2, 3, 4, 5, -6}) != 24 {
-		t.Fatal("The final result is: 24")
+	if sum1(4, []int{2, 1, 3, 4, 5, -5}) != 23 {
+		t.Fatal("The final result is: 23")
 	}
+
+	if sum1(0, []int{}) != 0 {
+		t.Fatal("The final result is: 0")
+	}
+
+	if sum1(0, []int{2, -5}) != 2 {
+		t.Fatal("The final result is: 2")
+	}
+
+	// if sum1(0, []int{-2, -1, -3, -4, -5, -5}) != 0 {
+	// 	t.Fatal("The final result is: 0")
+	// }
+
+	// if sum1(4, []int{2, 1, 3, 4, 5, 5}) != 0 {
+	// 	t.Fatal("The final result is: 0")
+	// }
 
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module C/Users/juuan/Documents/progra/Golang/githubProyectlab/golangProyectLab
+
+go 1.19
